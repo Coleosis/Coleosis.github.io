6c65ab92206b8e37c93db912f34ee819a38162ec
Coleosis <35709944+Coleosis@users.noreply.github.com>
2021-11-30 21:00:36 -0700

    Add files via upload

---
+++ FitnessBootStrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Cole Carlson
+    File Name: script.js
+    Date: 11/30/2021
+*/
+
+//jquery for hero image to consume the header window space
+$(document) .ready(function(){
+    $('.hero') .height($(window).height());
+});

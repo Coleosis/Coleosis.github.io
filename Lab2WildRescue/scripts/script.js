d22e7c2f814beaee4e770eff5eae6acf6e8679f9
Coleosis <35709944+Coleosis@users.noreply.github.com>
2021-12-01 21:48:45 -0700

    Add files via upload

---
+++ Lab2WildRescue/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Cole Carlson
+    File Name: script.js
+    Date: 12/01/2021
+*/
+
+//jQuery for hero image to consume the header window space
+$(document) .ready(function(){
+$('.hero') .height($(window) .height());
+});
